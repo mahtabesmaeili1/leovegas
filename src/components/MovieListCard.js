9cc7e4289f935a81036c20f9b4c1373d6e5387c6
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-05-07 10:31:14 +0200

    display the pictures and the title of the movies

--- src/components/MovieListCard.js
+++ src/components/MovieListCard.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 const MovieListCard = ({ movie }) => {
-  return <div>{movie.title}</div>;
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
+  console.log(movie);
+  return (
+    <div>
+      {movie.poster_path ? (
+        <img src={`${IMAGE_PATH}${movie.poster_path}`} alt="" />
+      ) : null}
+      <h5>{movie.title}</h5>
+    </div>
+  );
 };
 export default MovieListCard;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,30 +1,33 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import MovieListCard from "../components/MovieListCard";
 
 const HomePage = () => {
   const apiUrl = "https://api.themoviedb.org/3";
   const [movies, setMovies] = useState([]);
+  const getMovie = async () => {
+    const {
+      data: { results },
+    } = await axios.get(`${apiUrl}/discover/movie`, {
+      params: {
+        api_key: process.env.REACT_APP_TMDB_API_KEY,
+      },
+    });
+
+    setMovies(results);
+  };
 
   useEffect(() => {
-    const getMovie = async () => {
-      const {
-        data: { results },
-      } = await axios.get(`${apiUrl}/discover/movie`, {
-        params: {
-          api_key: process.env.REACT_APP_TMDB_API_KEY,
-        },
-      });
-      setMovies(results);
-    };
-
     getMovie();
   }, []);
+  const renderMovies = () =>
+    movies.map((movie) => <MovieListCard key={movie.id} movie={movie} />);
 
   return (
     <div>
-      {movies.map((movie) => (
-        <div key={movie.id}>{movie.title}</div>
-      ))}
+      <h1> hello </h1>
+
+      <div> {renderMovies()}</div>
     </div>
   );
 };
